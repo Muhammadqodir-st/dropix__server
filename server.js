a5f40bc7a39fdd3718d5863b68b21aedbc090911
Muhammadqodir-st <muhammadqodirov3609@example.com>
2025-08-21 13:28:41 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("backend start")
+});
+
+const PORT =process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
