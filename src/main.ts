a542d61040518ab022a3f0cbdc72c36256302607
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-11-14 15:26:03 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    origin:['*']
+    origin: ['http://localhost:3000']
   })
   await app.listen(process.env.PORT ?? 8000);
 }
