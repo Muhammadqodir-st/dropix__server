778e17cec56148e8a81df01c4326fbc9a8245c97
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-11-28 05:49:47 +0500

    fix

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(private readonly postService: PostService) { }
+
+    @Get()
+    findAll() {
+        return
+    }
+
+    @Get(":id")
+    findId() {
+        return
+    }
+
+    @Post("create")
+    createPost() {
+        return
+    }
+
+    @Patch(":id")
+    updateById() {
+        return
+    }
+
+    @Delete(":id")
+    deleteById() {
+        return
+    }
+
+    @Delete('delete')
+    deleteAll() {
+        return
+    }
+}
