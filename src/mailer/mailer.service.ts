4a446e5cf7c9dadc9830c4d9e3358f96d257b4f0
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-08 09:30:20 +0500

    fix

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -10,6 +10,8 @@ export class MailerService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             service: 'gmail',
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
