901806620c9fa535d054db0c7dbfd700316aa733
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-05-03 15:27:45 +0500

    update

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,44 +1,25 @@
 import { Injectable, Logger } from '@nestjs/common';
-import * as nodemailer from 'nodemailer'
-import { Transporter } from 'nodemailer'
+import { Resend } from 'resend';
 
 @Injectable()
 export class MailerService {
-    private transporter: Transporter
-    private logger = new Logger(MailerService.name)
-
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                rejectUnauthorized: false
-            },
-            debug: true,
-            logger: true
-        });
-    };
+    private resend = new Resend(process.env.RESEND_API_KEY);
+    private logger = new Logger(MailerService.name);
 
     async sendEmail(to: string, html: string, subject = 'Welcome to DROPIX') {
         try {
-            this.logger.log(`EMAIL_USER: ${process.env.EMAIL_USER}`);
-            const info = await this.transporter.sendMail({
-                from: `"DROPIX" <${process.env.EMAIL_USER}>`,
-                to,
-                subject,
-                html
+            const data = await this.resend.emails.send({
+                from: 'DROPIX <onboarding@resend.dev>', // Keyinchalik o'z domeningizni ulasangiz bo'ladi
+                to: [to],
+                subject: subject,
+                html: html,
             });
 
-            this.logger.log(`Email sent :${info.messageId}`)
-            return { success: true, messageId: info.messageId }
-        } catch (error: any) {
-            this.logger.error(`Email send failed:${error.message}`)
-            return { success: false, error: error.message }
+            this.logger.log(`Email sent successfully: ${data.data?.id}`);
+            return { success: true };
+        } catch (error:any) {
+            this.logger.error(`Resend failed: ${error.message}`);
+            return { success: false };
         }
     }
 }
